750c3930fff052c12c3b4f293d93abb8328e24c3
Steven Rummler <steven.rummler@churchofjesuschrist.org>
2020-06-03 15:53:21 -0600

    edited purchases table script

--- databases/flyway_demo/V1.2__add_purchases_table.sql
+++ databases/flyway_demo/V1.2__add_purchases_table.sql
@@ -1,7 +1,8 @@
 create table purchases (
   id integer not null auto_increment,
   user_id integer not null,
-  amount decimal(10,2),
+  price decimal(10,2),
+  tax decimal(10,2),
   constraint id_pk primary key (id),
   FOREIGN KEY (user_id)
         REFERENCES users(id)
