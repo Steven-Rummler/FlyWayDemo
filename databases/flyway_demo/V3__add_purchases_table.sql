5660f636a84fb024c3c07fc2ba4b77c0e38a6b99
Steven Rummler <steven.rummler@churchofjesuschrist.org>
2020-06-03 16:27:57 -0600

    merging updates from master

--- databases/flyway_demo/V3__add_purchases_table.sql
+++ databases/flyway_demo/V3__add_purchases_table.sql
@@ -1,7 +1,11 @@
 create table purchases (
   id integer not null auto_increment,
   user_id integer not null,
+<<<<<<< HEAD
   amount decimal(10,2),
+=======
+  amount number,
+>>>>>>> 03de433... added purchases table
   constraint id_pk primary key (id),
   FOREIGN KEY (user_id)
         REFERENCES users(id)
