b623fb98ee552626fbc6b40a6c185e8e9fe8756d
Steven Rummler <steven.rummler@churchofjesuschrist.org>
2020-06-09 13:13:33 -0600

    Fixed T-SQL foreign key syntax

--- migrations/V1.2.1__add_lobbyist_politician_foreign_key.sql
+++ migrations/V1.2.1__add_lobbyist_politician_foreign_key.sql
@@ -3,5 +3,4 @@ ADD politician_id integer;
 
 ALTER TABLE lobbyists
 ADD CONSTRAINT lobbyist_politician FOREIGN KEY (politician_id)
-        REFERENCES politicians(id)
-        ON DELETE RESTRICT;
+        REFERENCES politicians(id);
